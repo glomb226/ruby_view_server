3ebd90583de3195d5e20f130d30b1ebe1ccb8f26
luis martinez <LuisMtzJr@luiss-MacBook-Air.local>
2014-11-13 16:23:30 -0600

    Section 1 complete

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,16 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+name = "Glomb"
+job = "zookeeper"
+string = ERB.new "My name is <%= name%>, and I am a <%=job %>"
+puts string.result(binding)
+
+animals = {cow: 'moo', dog: 'woof', cat: 'meow'}
+hash = ERB.new '
+<% animals.each do |key, value| %>
+   <%= "I love #{key}s, and they go #{value}" %>
+<% end %>'
+
+puts hash.result(binding)
